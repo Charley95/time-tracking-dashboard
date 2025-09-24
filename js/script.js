1e57b5e857e2682d488013fcd18941af4cd4f76c
Chinenye <ibenyenwachinenye07@gmail.com>
2025-09-24 01:42:10 +0100

    also added a leading dot to the json file path in my fetch method

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Fetch the JSON data and initialize the UI
-    fetch('/data.json')
+    fetch('./data.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
